d2889f9519536a146b5719114a9c8417a95da3d2
Maxime Costalonga <maxime.costalonga.external@eviden.com>
2025-02-07 16:45:37 +0000

    doc: add basic example

---
+++ examples/basic.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+import anafit
+
+plt.ion()
+fig = plt.figure()
+ax = fig.add_subplot(111)
+(my_line,) = ax.plot(
+    np.arange(0, 100, 1),
+    np.arange(50, 250, 2) + 10 * (np.random.rand(100) - 1 / 2),
+    "b+",
+)
+ax.plot(
+    np.arange(0, 100, 1),
+    np.arange(0, 300, 3) + 30 * (np.random.rand(100) - 1 / 2),
+    "rx",
+)
+ax.plot(
+    np.arange(0, 100, 1),
+    np.square(np.arange(0, 10, 0.1)) + 5 * (np.random.rand(100) - 1 / 2),
+    "k.",
+)
+ana = anafit.Figure(fig)
+plt.show(block=True)
